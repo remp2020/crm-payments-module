c2c80545d3c3c7c39ba537a9c8df659bda931f6f
Peter Dulacka <dulacka@gmail.com>
2019-03-06 14:58:34 +0100

    Payment counts criteria should be based on users, not payments

--- src/segment/PaymentCountsCriteria.php
+++ src/segment/PaymentCountsCriteria.php
@@ -45,12 +45,12 @@ class PaymentCountsCriteria implements CriteriaInterface
         $join = [];
 
         if ($paramBag->has('subscription_payments')) {
-            $join[] = "LEFT JOIN user_meta spum ON `key` = 'subscription_payments' AND spum.user_id = payments.user_id";
-            $where += $paramBag->number('subscription_payments')->escapedConditions('spum.value');
+            $join[] = "LEFT JOIN user_meta spum ON `key` = 'subscription_payments' AND spum.user_id = users.id";
+            $where += $paramBag->number('subscription_payments')->conditions('COALESCE(`spum`.`value`, 0)');
         }
 
-        return "SELECT DISTINCT(payments.user_id) AS id, " . Fields::formatSql($this->fields()) . "
-          FROM payments " .
+        return "SELECT DISTINCT(users.id) AS id, " . Fields::formatSql($this->fields()) . "
+          FROM users " .
           implode("\n", $join) . "
           WHERE " . implode(" AND ", $where);
     }
@@ -68,15 +68,7 @@ class PaymentCountsCriteria implements CriteriaInterface
     public function fields(): array
     {
         return [
-            'payments.id' => 'payment_id',
-            'payments.status' => 'payment_status',
-            'payments.variable_symbol' => 'variable_symbol',
-            'payments.amount' => 'amount',
-            'payments.additional_amount' => 'additional_amount',
-            'payments.additional_type' => 'additional_type',
-            'payments.created_at' => 'created_at',
-            'payments.paid_at' => 'paid_at',
-            'payments.recurrent_charge' => 'recurrent_charge',
+            'users.id' => 'payment_id',
         ];
     }
 }
