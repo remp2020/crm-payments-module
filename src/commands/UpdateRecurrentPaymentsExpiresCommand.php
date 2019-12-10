e2db2ed86d5a6346ba723a5b0313e09a82ac92b7
Peter Dulacka <dulacka@gmail.com>
2019-12-10 15:03:37 +0100

    Adding gateway filtering to cid expiration update command
    remp/crm#681

--- src/commands/UpdateRecurrentPaymentsExpiresCommand.php
+++ src/commands/UpdateRecurrentPaymentsExpiresCommand.php
@@ -4,6 +4,7 @@ namespace Crm\PaymentsModule\Commands;
 
 use Crm\PaymentsModule\GatewayFactory;
 use Crm\PaymentsModule\Gateways\RecurrentPaymentInterface;
+use Crm\PaymentsModule\Repository\PaymentGatewaysRepository;
 use Crm\PaymentsModule\Repository\RecurrentPaymentsRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -16,20 +17,33 @@ class UpdateRecurrentPaymentsExpiresCommand extends Command
 
     private $gatewayFactory;
 
+    private $paymentGatewaysRepository;
+
     public function __construct(
         RecurrentPaymentsRepository $recurrentPaymentsRepository,
-        GatewayFactory $gatewayFactory
+        GatewayFactory $gatewayFactory,
+        PaymentGatewaysRepository $paymentGatewaysRepository
     ) {
         parent::__construct();
         $this->recurrentPaymentsRepository = $recurrentPaymentsRepository;
         $this->gatewayFactory = $gatewayFactory;
+        $this->paymentGatewaysRepository = $paymentGatewaysRepository;
     }
 
     protected function configure()
     {
         $this->setName('payments:update_recurrent_payments_expires')
             ->setDescription('Update recurrent payments expires')
-            ->addOption('count', null, InputOption::VALUE_REQUIRED, 'Number of cids to process');
+            ->addOption(
+                'count',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Number of cids to process')
+            ->addOption(
+                'gateway',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Payment gateway code to check use. If not specified, all recurring gateways are checked.');
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
@@ -45,7 +59,19 @@ class UpdateRecurrentPaymentsExpiresCommand extends Command
             $count = intval($input->getOption('count'));
         }
 
-        $recurrentPayments = $this->recurrentPaymentsRepository->all()->where(['expires_at' => null])->limit($count);
+        $recurrentPayments = $this->recurrentPaymentsRepository->all()->where([
+            'expires_at' => null,
+            'state' => RecurrentPaymentsRepository::STATE_ACTIVE,
+        ])->limit($count);
+
+        if ($code = $input->getOption('gateway')) {
+            $gateway = $this->paymentGatewaysRepository->findByCode($code);
+            if (!$gateway) {
+                $output->writeln("<error>ERROR: gateway <info>{$code}</info> doesn't exist:</error>");
+                return;
+            }
+            $recurrentPayments->where(['payment_gateway_id' => $gateway->id]);
+        }
 
         $gateways = [];
         foreach ($recurrentPayments as $recurrentPayment) {
@@ -61,6 +87,9 @@ class UpdateRecurrentPaymentsExpiresCommand extends Command
             if (!$gateway instanceof RecurrentPaymentInterface) {
                 $output->writeln("<error>Error: gateway {$code} does not implement RecurrentPaymentInterface</error>");
             }
+            $paymentGateway = $this->paymentGatewaysRepository->findByCode($code);
+
+            $output->writeln("Checking <comment>{$paymentGateway->name}</comment> expirations:");
 
             try {
                 $result = $gateway->checkExpire(array_keys($recurrentPayments));
@@ -69,10 +98,10 @@ class UpdateRecurrentPaymentsExpiresCommand extends Command
                     $this->recurrentPaymentsRepository->update($recurrentPayment, [
                         'expires_at' => $expire,
                     ]);
-                    $output->writeln('<info>Card_id: ' . $recurrentPayment->cid . ' User_id: ' . $recurrentPayment->user_id . ' Expires_at: ' . $expire->format('Y-m-d H:i:s') . '</info>');
+                    $output->writeln('  * CID: ' . $recurrentPayment->cid . ' User ID: ' . $recurrentPayment->user_id . ' Expires at: ' . $expire->format('Y-m-d H:i:s') . '</info>');
                 }
             } catch (\Exception $e) {
-                $output->writeln("<error>Error {$e->getMessage()}</error>");
+                $output->writeln("  * <error>Error {$e->getMessage()}</error>");
             }
         }
 
