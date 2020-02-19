6b55f6d6d483ba9523f16576be91339c389fb432
Levente Besenyei <lbesenyei@brainsum.com>
2020-02-19 16:24:26 +0100

    Removed unused imports, and unnecessary constructor override. Added simple redirect to the begin method. Switched to lowercase true for consistnecy.

--- src/model/Gateway/Free.php
+++ src/model/Gateway/Free.php
@@ -7,27 +7,10 @@ use Crm\PaymentsModule\Repository\PaymentMetaRepository;
 use Nette\Application\LinkGenerator;
 use Nette\Http\Response;
 use Nette\Localization\ITranslator;
-use Omnipay\Omnipay;
-use Omnipay\PayPal\ExpressGateway;
 use Crm\PaymentsModule\Gateways\GatewayAbstract;
 
 class Free extends GatewayAbstract
 {
-    /** @var ExpressGateway */
-    protected $gateway;
-
-    private $paymentMetaRepository;
-
-    public function __construct(
-        LinkGenerator $linkGenerator,
-        ApplicationConfig $applicationConfig,
-        Response $httpResponse,
-        PaymentMetaRepository $paymentMetaRepository,
-        ITranslator $translator
-    ) {
-        parent::__construct($linkGenerator, $applicationConfig, $httpResponse, $translator);
-        $this->paymentMetaRepository = $paymentMetaRepository;
-    }
 
     protected function initialize()
     {
@@ -35,11 +18,15 @@ class Free extends GatewayAbstract
 
     public function begin($payment)
     {
-
+        $url = $this->generateReturnUrl($payment, [
+            'vs' => $payment->variable_symbol
+        ]);
+        $this->httpResponse->redirect($url);
+        exit();
     }
 
     public function complete($payment): ?bool
     {
-        return TRUE;
+        return true;
     }
 }
