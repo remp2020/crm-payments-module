3b398352493b91a6f812921db210c54f5470d6b6
Dominik Skerhak <dominik.skerhak@gmail.com>
2020-04-22 10:22:33 +0200

    Allow to stop recurrent profile after charge failed
    Issue: User is unable to stop recurrent payment after charge failed.
    
    Reason is that system sets recurrent profile to state `charge_failed`
    and creates new payment with new recurrent profile. Unfortunatelly
    payment in customer zone points to old recurrent profile (probably
    needs refactoring) and because of that user was not able to stop
    (current) recurrent profile or to see if that particular payment will
    be charged in future.
    
    Fix: "Resolve" recurrent profile if it is in 'charge_failed' state and
    find new valid one. Show state and possible user actions against this
    valid recurrent payment profile.
    
    Added also dates of last failed and next charge attempt.
    
    remp/crm#1163

--- src/model/RecurrentPaymentsResolver.php
+++ src/model/RecurrentPaymentsResolver.php
@@ -7,6 +7,8 @@ use Crm\PaymentsModule\Repository\PaymentsRepository;
 use Crm\PaymentsModule\Repository\RecurrentPaymentsRepository;
 use Crm\SubscriptionsModule\Repository\SubscriptionTypesRepository;
 use Nette\Database\Table\ActiveRow;
+use Nette\Utils\DateTime;
+use Tracy\Debugger;
 
 class RecurrentPaymentsResolver
 {
@@ -15,6 +17,8 @@ class RecurrentPaymentsResolver
     private $recurrentPaymentsRepository;
 
     private $subscriptionTypesRepository;
+
+    public $lastFailedChargeAt = null;
 
     public function __construct(
         PaymentsRepository $paymentsRepository,
@@ -75,5 +79,38 @@ class RecurrentPaymentsResolver
             }
         }
         return $amount;
+    }
+
+    /**
+     * resolveFailedRecurrent checks following recurring payments after charge failed and returns last recurring payment.
+     *
+     * This method follows sequence:
+     * - get $recurrentPayment->payment
+     * - get $payment->recurringPayment (via parent_payment_id)
+     *
+     * So methods always follows original payment and it doesn't jump to recurrent payment of other subscription.
+     *
+     * If resolved is unable to find following recurring profile, it returns same payment.
+     */
+    public function resolveFailedRecurrent(ActiveRow $recurrentPayment): ActiveRow
+    {
+        if ($recurrentPayment->state === RecurrentPaymentsRepository::STATE_CHARGE_FAILED) {
+            $this->lastFailedChargeAt = $recurrentPayment->charge_at;
+            if (isset($recurrentPayment->payment) && ($nextRecurrent = $this->recurrentPaymentsRepository->recurrent($recurrentPayment->payment))) {
+                $recurrentPayment = $this->resolveFailedRecurrent($nextRecurrent);
+            } else {
+                Debugger::log('Unable to find next payment for failed recurrent ID [' . $recurrentPayment->id . ']', Debugger::ERROR);
+            }
+        }
+        return $recurrentPayment;
+    }
+
+    public function getLastChargeFailedDateTime(): DateTime
+    {
+        if ($this->lastFailedChargeAt === null) {
+            throw new \Exception('No last charge_failed date is set. Did you call `resolveFailedRecurrent()`?');
+        }
+
+        return new DateTime($this->lastFailedChargeAt);
     }
 }
