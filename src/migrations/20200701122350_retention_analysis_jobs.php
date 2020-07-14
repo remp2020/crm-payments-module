b0a2b411f751b2b3e304a7c931e8881d675790f7
Miroslav Svítok <mirosvitok@gmail.com>
2020-07-14 16:00:46 +0200

    Fixed migration - specification of mediumtext for Phinx

--- src/migrations/20200701122350_retention_analysis_jobs.php
+++ src/migrations/20200701122350_retention_analysis_jobs.php
@@ -18,7 +18,7 @@ class RetentionAnalysisJobs extends AbstractMigration
             ])
             ->addColumn('name', 'string', ['null' => false])
             ->addColumn('params', 'text', ['null' => false])
-            ->addColumn('results', 'mediumtext', ['null' => true])
+            ->addColumn('results', 'text', ['null' => true, 'limit' => 'TEXT_MEDIUM'])
             ->addColumn('started_at', 'datetime', ['null' => true])
             ->addColumn('finished_at', 'datetime', ['null' => true])
             ->addColumn('updated_at', 'datetime', ['null' => false])
