5429b8973f6897123427e8e2034b41a2f33b25b5
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-04-20 23:19:16 +0200

    Optimize SQL in payments:calculate_averages command
    SQL queries were optimized to be less (DB) memory & time consuming.
    Results of calculations are same.
    
    ---
    
    Issue: We are getting lock timeout:
    
    ```
    General error: 1205 Lock wait timeout exceeded; try restarting transaction
    ```
    
    ---
    
    Change: Most of the users have entry for these calculations so almost
    all rows were updated and `INSERT INTO ... ON DUPLICATE` doesn't make
    sense here (since most of the "inserts" were "updates").
    
    Instead of `INSERT INTO ... ON DUPLICATE` we now first fill zero values
    for all new users (using `INSERT IGNORE`) and then run simple update for
    all users with product payments.
    
    Test sample database (~500.000 users, ~10.000 product payments):
    - Before change: ~250s (~4 minutes).
    - After change: ~60s (~1 minute).
    
    remp/crm#1835

--- src/commands/CalculateAveragesCommand.php
+++ src/commands/CalculateAveragesCommand.php
@@ -35,62 +35,90 @@ class CalculateAveragesCommand extends \Symfony\Component\Console\Command\Comman
         $subscriptionTypeItem = SubscriptionTypePaymentItem::TYPE;
 
         $this->database->query(<<<SQL
-            INSERT INTO user_meta (`user_id`,`key`,`value`,`created_at`,`updated_at`)
-            SELECT
-                id,
-                'subscription_payments',
-                (
-                    SELECT COALESCE(COUNT(DISTINCT(payments.id)), 0)
-                    FROM payments
-                    INNER JOIN payment_items ON payment_items.payment_id = payments.id AND payment_items.type IN ('$subscriptionTypeItem')
-                    WHERE
-                        status='$paidStatus'
-                        AND payments.user_id = users.id
-                ),
-                NOW(),
-                NOW()
-            FROM users
-            ON DUPLICATE KEY UPDATE `updated_at`=NOW(), `value`=VALUES(value);
+            -- fill empty values for new users
+            INSERT IGNORE INTO `user_meta` (`user_id`,`key`,`value`, `created_at`, `updated_at`)
+            SELECT `id`, 'subscription_payments', 0, NOW(), NOW()
+            FROM `users`;
+
+            -- calculate & update values
+            UPDATE `user_meta`
+            INNER JOIN (
+                SELECT
+                    `payments`.`user_id` AS `user_id`,
+                    COUNT(DISTINCT(`payments`.`id`)) AS `subscription_payments_count`
+                FROM `payment_items`
+                INNER JOIN `payments`
+                    ON `payments`.`id` = `payment_items`.`payment_id`
+                    AND `payments`.`status` = '$paidStatus'
+                WHERE `payment_items`.`type` IN ('$subscriptionTypeItem')
+                GROUP BY `payments`.`user_id`
+            ) AS `subscription_payments`
+                ON `user_meta`.`user_id` = `subscription_payments`.`user_id`
+            SET
+               `value` = `subscription_payments`.`subscription_payments_count`,
+               `updated_at` = NOW()
+            WHERE
+                `key` = 'subscription_payments'
+            ;
 SQL
         );
 
         $this->database->query(<<<SQL
-            INSERT INTO user_meta (`user_id`,`key`,`value`,`created_at`,`updated_at`)
-            SELECT
-                id,
-                'subscription_payments_amount',
-                (
-                    SELECT COALESCE(SUM(payment_items.amount * payment_items.count), 0)
-                    FROM payments
-                    INNER JOIN payment_items ON payment_items.payment_id = payments.id AND payment_items.type IN ('$subscriptionTypeItem')
-                    WHERE
-                        status='$paidStatus'
-                        AND payments.user_id = users.id
-                ),
-                NOW(),
-                NOW()
-            FROM users
-            ON DUPLICATE KEY UPDATE `updated_at`=NOW(), `value`=VALUES(value);
+            -- fill empty values for new users
+            INSERT IGNORE INTO `user_meta` (`user_id`,`key`,`value`, `created_at`, `updated_at`)
+            SELECT `id`, 'subscription_payments_amount', 0, NOW(), NOW()
+            FROM `users`;
+
+            -- calculate & update values
+            UPDATE `user_meta`
+            INNER JOIN (
+                SELECT
+                    `payments`.`user_id` AS `user_id`,
+                    COALESCE(SUM(`payment_items`.`amount` * `payment_items`.`count`), 0) AS `subscription_payments_amount`
+                FROM `payment_items`
+                INNER JOIN `payments`
+                    ON `payments`.`id` = `payment_items`.`payment_id`
+                    AND `payments`.`status` = '$paidStatus'
+                WHERE `payment_items`.`type` IN ('$subscriptionTypeItem')
+                GROUP BY `payments`.`user_id`
+            ) AS `subscription_payments`
+                ON `user_meta`.`user_id` = `subscription_payments`.`user_id`
+            SET
+               `value` = `subscription_payments`.`subscription_payments_amount`,
+               `updated_at` = NOW()
+            WHERE
+                `key` = 'subscription_payments_amount'
+            ;
 SQL
         );
 
         $this->database->query(<<<SQL
-            INSERT INTO user_meta (`user_id`,`key`,`value`,`created_at`,`updated_at`)
-            SELECT
-                id,
-                'avg_month_payment',
-                (
-                    SELECT COALESCE(AVG(payments.amount / subscription_types.length * 31), 0)
-                    FROM payments
-                    INNER JOIN subscription_types ON subscription_types.id = payments.subscription_type_id AND subscription_types.length > 0
-                    WHERE
-                        payments.status='$paidStatus'
-                        AND payments.user_id = users.id
-                ),
-                NOW(),
-                NOW()
-            FROM users
-            ON DUPLICATE KEY UPDATE `updated_at`=NOW(), `value`=VALUES(value);
+            -- fill empty values for new users
+            INSERT IGNORE INTO `user_meta` (`user_id`,`key`,`value`, `created_at`, `updated_at`)
+            SELECT `id`, 'avg_month_payment', 0, NOW(), NOW()
+            FROM `users`;
+
+            -- calculate & update values
+            UPDATE `user_meta`
+            INNER JOIN (
+                SELECT
+                    `payments`.`user_id` AS `user_id`,
+                    COALESCE(AVG((`payments`.`amount` / `subscription_types`.`length`) * 31), 0) AS `avg_month_payment_amount`
+                FROM `payments`
+                INNER JOIN `subscription_types`
+                   ON `subscription_types`.`id` = `payments`.`subscription_type_id`
+                   AND `subscription_types`.`length` > 0
+                WHERE
+                    `payments`.`status` = '$paidStatus'
+                GROUP BY `payments`.`user_id`
+            ) AS `avg_month_payment`
+                ON `user_meta`.`user_id` = `avg_month_payment`.`user_id`
+            SET
+               `value` = `avg_month_payment`.`avg_month_payment_amount`,
+               `updated_at` = NOW()
+            WHERE
+                `key` = 'avg_month_payment'
+            ;
 SQL
         );
 
