56644eb92d1cf999631a3d925b5a3cf1bc6abae3
Matus Kalafut <kalafut.matus@gmail.com>
2022-07-12 10:51:33 +0000

    Add universal search to search across whole application.
    - Add `UniversalSearchWidget` to display search box in header on all admin pages.
      
    - Add `UniversalSearchDataProviderInterface` data provider to implement custom search models in modules.
    
    - Add `admin.dataprovider.universal_search` data provider placeholder to register custom search models.
    
    - Add `UniversalSearchDataProvider` in `PaymentsModule` for custom searches in payments
    using variable symbol.
    
    - Add `UniversalSearchDataProvider` in `UsersModule` for custom searches in users
    using `user.id` and `user.email`. For email field uses wildcard search
    after entering 3 or more characters.
    
    remp/crm#2485

---
+++ src/DataProviders/UniversalSearchDataProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Crm\PaymentsModule\DataProvider;
+
+use Crm\AdminModule\Model\UniversalSearchDataProviderInterface;
+use Crm\ApplicationModule\Helpers\UserDateHelper;
+use Crm\PaymentsModule\Repository\PaymentsRepository;
+use Kdyby\Translation\Translator;
+use Nette\Application\LinkGenerator;
+
+class UniversalSearchDataProvider implements UniversalSearchDataProviderInterface
+{
+    private PaymentsRepository $paymentsRepository;
+    private LinkGenerator $linkGenerator;
+    private Translator $translator;
+    private UserDateHelper $userDateHelper;
+
+    public function __construct(
+        PaymentsRepository $paymentsRepository,
+        LinkGenerator $linkGenerator,
+        Translator $translator,
+        UserDateHelper $userDateHelper
+    ) {
+        $this->paymentsRepository = $paymentsRepository;
+        $this->linkGenerator = $linkGenerator;
+        $this->translator = $translator;
+        $this->userDateHelper = $userDateHelper;
+    }
+
+    public function provide(array $params): array
+    {
+        $result = [];
+        $term = $params['term'];
+        $groupName = $this->translator->translate('payments.data_provider.universal_search.payment_group');
+
+        $payments = $this->paymentsRepository->findAllByVS($term)
+            ->order('paid_at DESC')
+            ->fetchAll();
+        foreach ($payments as $payment) {
+            $text = "{$payment->user->email} - {$payment->variable_symbol}";
+            if ($payment->paid_at) {
+                $text .= ' - ' . $this->userDateHelper->process($payment->paid_at);
+            }
+            $result[$groupName][] = [
+                'id' => 'payment_' . $payment->id,
+                'text' => $text,
+                'url' => $this->linkGenerator->link('Users:UsersAdmin:show', ['id' => $payment->user_id])
+            ];
+        }
+
+        return $result;
+    }
+}
