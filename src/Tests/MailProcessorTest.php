5db54bab43cdf590f9e7fb0d9f5c54d837fa905b
Dominik Skerhak <dominik.skerhak@gmail.com>
2023-01-10 17:21:04 +0100

    Test confirmation from bank statement if two valid VS are present
    For peace of mind, adding test to confirm that VS returned from library
    is used properly also in case there is valid VS (of second payment) in
    receiver message.
    
    remp/crm#2603

--- src/Tests/MailProcessorTest.php
+++ src/Tests/MailProcessorTest.php
@@ -232,6 +232,44 @@ class MailProcessorTest extends PaymentsTestCase
         $this->assertEquals(PaymentsRepository::STATUS_PAID, $newPayment->status);
     }
 
+    // should test that VS set by library has precedence over VS found by MailProcessor in receiver message
+    public function testTwoPaymentsTwoVariableSymbolsOneInVsOneInReceiverMessage()
+    {
+        $variableSymbolPayment1 = '7492857611';
+        $variableSymbolPayment2 = '7492857612';
+        $amount = 10.2;
+
+        $payment1 = $this->createPayment($variableSymbolPayment1);
+        $this->paymentsRepository->update($payment1, array('amount' => $amount));
+
+        $payment2 = $this->createPayment($variableSymbolPayment2);
+        $this->paymentsRepository->update($payment2, array('amount' => $amount));
+
+        $paidPayments = $this->paymentsRepository->getTable()->where('status = ?', PaymentsRepository::STATUS_PAID)->count('*');
+        $this->assertEquals(0, $paidPayments); // zero paid payments
+
+        $mailContent = new MailContent();
+        $mailContent->setAmount($amount);
+        $mailContent->setTransactionDate(strtotime('2.3.2015 13:43'));
+        $mailContent->setVs($variableSymbolPayment1); // correct VS for first payment; should be used to confirm payment
+        $mailContent->setReceiverMessage($variableSymbolPayment2); // correct VS for second payment; shouldn't be used
+
+        $result = $this->mailProcessor->processMail($mailContent, new TestOutput());
+        $this->assertTrue($result);
+
+        $log = $this->parsedMailLogsRepository->lastLog();
+        $this->assertEquals(ParsedMailLogsRepository::STATE_CHANGED_TO_PAID, $log->state);
+
+        $paidPayments = $this->paymentsRepository->getTable()->where('status = ?', PaymentsRepository::STATUS_PAID)->count('*');
+        $this->assertEquals(1, $paidPayments); // one one payment is paid
+
+        $firstPayment = $this->paymentsRepository->find($payment1->id);
+        $secondPayment = $this->paymentsRepository->find($payment2->id);
+
+        $this->assertEquals(PaymentsRepository::STATUS_PAID, $firstPayment->status); // confirmed / paid by mail processor
+        $this->assertEquals(PaymentsRepository::STATUS_FORM, $secondPayment->status); // not confirmed by mail processor
+    }
+
     public function testRepeatedPaymentFromBank()
     {
         $variableSymbol = '7492857611';
