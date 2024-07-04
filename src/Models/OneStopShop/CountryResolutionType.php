86fdf644b2b4f8a931545e77dbca3b6378b6eb68
Miroslav Svitok <mirosvitok@gmail.com>
2024-07-04 16:47:26 +0200

    Fix OSS Dennikn data provider - resolve non-associated print addresses
    Specific for Dennikn how print addresses are selected
    
    remp/crm#3241

--- src/Models/OneStopShop/CountryResolutionType.php
+++ src/Models/OneStopShop/CountryResolutionType.php
@@ -7,6 +7,7 @@ enum CountryResolutionType: string
     case IP_ADDRESS = 'ip_address';
     case INVOICE_ADDRESS = 'invoice_address';
     case PAYMENT_ADDRESS = 'payment_address';
+    case PRINT_ADDRESS = 'print_address';
     case USER_SELECTED = 'user_selected';
     case ADMIN_SELECTED = 'admin_selected';
     case PREVIOUS_PAYMENT = 'previous_payment';
