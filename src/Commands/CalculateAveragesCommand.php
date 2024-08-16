8a086a72dd4089121bff525ec6f7b80d148f5878
Dominik Skerhak <dominik.skerhak@gmail.com>
2024-08-16 14:33:05 +0200

    Add configuration options to calculate_averages commands
    Affected commands:
    
    - payments:calculate_averages
    - products:calculate_averages
    
    ---
    
    New option `--subscription_period` added into `payments:calculate_averages`
    command. Allows to set number of days in the basic monthly subscription when
    calculating the average monthly payment.
    
    Can be configured also in neon config - use `setSubscriptionPeriod(28)` in
    the setup directive of `paymentsCalculateAveragesCommand`.
    
    ---
    
    Added option `--minimal_subscription_length` to `payments:calculate_averages`
    command. Allows to set minimal length (in days) of subscription types which
    should be included in calculations.
    
    Can be configured also in neon config - use `setMinimalSubscriptionLength(2)` in
    the setup directive of `paymentsCalculateAveragesCommand`.
    
    ---
    
    New option `--calculated_period` added into `payments:calculate_averages`
    and `products:calculate_averages` commands. Use DateInterval format to set
    period. Eg. `P2Y` will take only payments in the last two years into
    account when calculating averages.
    
    Can be configured also in neon config - use `setCalculatedPeriod('P2Y)` in
    the setup directive of both commands - `paymentsCalculateAveragesCommand`
    and `productsCalculateAveragesCommand`.
    
    ---
    
    Co-authored-by: Peter Dulacka <dulacka@gmail.com>
    
    remp/crm#3139

--- src/Commands/CalculateAveragesCommand.php
+++ src/Commands/CalculateAveragesCommand.php
@@ -8,6 +8,8 @@ use Crm\SubscriptionsModule\Models\PaymentItem\SubscriptionTypePaymentItem;
 use Crm\UsersModule\Repositories\UserMetaRepository;
 use Crm\UsersModule\Repositories\UserStatsRepository;
 use Crm\UsersModule\Repositories\UsersRepository;
+use DateInterval;
+use DateTime;
 use Nette\Database\Explorer;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -21,11 +23,18 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class CalculateAveragesCommand extends Command
 {
-    private const PAYMENT_STATUSES = [PaymentsRepository::STATUS_PAID, PaymentsRepository::STATUS_PREPAID];
-
     use DecoratedCommandTrait;
 
+    private const PAYMENT_STATUSES = [PaymentsRepository::STATUS_PAID, PaymentsRepository::STATUS_PREPAID];
+
+    private int $subscriptionPeriod = 31;
+    private int $minimalSubscriptionLength = 1;
+    private ?int $calculatedPeriod = null;
+    private ?DateTime $startDate = null;
+
     private Explorer $database;
+
+    private PaymentsRepository $paymentsRepository;
 
     private UserStatsRepository $userStatsRepository;
 
@@ -35,12 +44,14 @@ class CalculateAveragesCommand extends Command
 
     public function __construct(
         Explorer $database,
+        PaymentsRepository $paymentsRepository,
         UserStatsRepository $userStatsRepository,
         UsersRepository $usersRepository,
         UserMetaRepository $userMetaRepository
     ) {
         parent::__construct();
         $this->database = $database;
+        $this->paymentsRepository = $paymentsRepository;
         $this->userStatsRepository = $userStatsRepository;
         $this->usersRepository = $usersRepository;
         $this->userMetaRepository = $userMetaRepository;
@@ -61,12 +72,80 @@ class CalculateAveragesCommand extends Command
                 null,
                 InputOption::VALUE_REQUIRED,
                 "Compute average values for given user only."
-            );
+            )
+            ->addOption(
+                'subscription_period',
+                null,
+                InputOption::VALUE_REQUIRED,
+                "Set number of days in month (basic monthly subscription). Useful if monthly subscriptions' length is 28 or 30 days. Default is 31.",
+            )
+            ->addOption(
+                'minimal_subscription_length',
+                null,
+                InputOption::VALUE_REQUIRED,
+                "Set minimal length (in days) of subscription type to include in calculation. Default is 1.",
+            )
+            ->addOption(
+                'calculated_period',
+                null,
+                InputOption::VALUE_REQUIRED,
+                "Set beginning for which should be averages calculated. Use PHP's DateInterval format (eg. P2Y). Default: no beginning; will calculate from all payments.",
+            )
+        ;
+    }
+
+    public function setSubscriptionPeriod(int $days): void
+    {
+        $this->subscriptionPeriod = $days;
+    }
+
+    public function setMinimalSubscriptionLength(int $days): void
+    {
+        $this->minimalSubscriptionLength = $days;
+    }
+
+    public function setCalculatedPeriod(int $days): void
+    {
+        $this->calculatedPeriod = $days;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $keys = ['subscription_payments', 'subscription_payments_amount', 'avg_month_payment'];
+
+        // if set, option subscription_period overrides config and default value
+        $subscriptionPeriod = $input->getOption('subscription_period');
+        if ($subscriptionPeriod !== null) {
+            $this->subscriptionPeriod = (int) $subscriptionPeriod;
+        }
+        $this->line("  * Subscription period set to: <comment>{$this->subscriptionPeriod}</comment>.");
+
+        // if set, option calculated_period overrides config and default value (no period)
+        $calculatedPeriod = $input->getOption('calculated_period');
+        if ($calculatedPeriod !== null) {
+            $this->calculatedPeriod = (int) $calculatedPeriod;
+        }
+
+        if ($this->calculatedPeriod !== null) {
+            $interval = new DateInterval("P{$this->calculatedPeriod}D");
+            $this->startDate = (new DateTime())->sub($interval);
+        } else {
+            $firstPaidAt = $this->paymentsRepository->getTable()->where(['paid_at IS NOT NULL'])->order('paid_at ASC')->limit(1)->fetch();
+            $this->startDate = $firstPaidAt?->paid_at;
+        }
+
+        if ($this->startDate === null) {
+            $this->error('Unable to find paid payments. Nothing done.');
+            return Command::FAILURE;
+        }
+        $this->line('  * Including all subscription payments paid after: <comment>' . $this->startDate->format(DATE_RFC3339) . '</comment>.');
+
+        // if set, option minimal_subscription_length overrides config and default value
+        $minimalSubscriptionLength = $input->getOption('minimal_subscription_length');
+        if ($minimalSubscriptionLength !== null) {
+            $this->minimalSubscriptionLength = (int) $minimalSubscriptionLength;
+        }
+        $this->line("  * Minimal subscription length set to: <comment>{$this->minimalSubscriptionLength}</comment>.");
 
         if ($input->getOption('delete')) {
             $this->line("  * deleting old values from 'user_stats' and 'user_meta' tables.");
@@ -137,19 +216,26 @@ SQL, $key);
     {
         $this->line("  * computing '<info>subscription_payments</info>' for user IDs between [<info>{$minUserId}</info>, <info>{$maxUserId}</info>]");
 
+        $paymentPaidAt = $this->startDate;
         $subscriptionTypeItem = SubscriptionTypePaymentItem::TYPE;
 
         $values = $this->database->query(<<<SQL
-                SELECT
-                    `payments`.`user_id` AS `user_id`,
-                    COUNT(DISTINCT(`payments`.`id`)) AS `subscription_payments`
-                FROM `payment_items`
-                INNER JOIN `payments`
-                    ON `payments`.`id` = `payment_items`.`payment_id`
-                    AND `payments`.`status` IN (?)
-                WHERE `payment_items`.`type` IN (?) AND `payments`.`user_id` BETWEEN ? AND ?
-                GROUP BY `payments`.`user_id`
-SQL, self::PAYMENT_STATUSES, $subscriptionTypeItem, $minUserId, $maxUserId)
+            SELECT
+                `payments`.`user_id` AS `user_id`,
+                COUNT(DISTINCT(`payments`.`id`)) AS `subscription_payments`
+            FROM `payment_items`
+            INNER JOIN `payments`
+                ON `payments`.`id` = `payment_items`.`payment_id`
+                AND `payments`.`status` IN (?)
+                AND `payments`.`paid_at` > ?
+            INNER JOIN `subscription_types`
+                ON `subscription_types`.`id` = `payments`.`subscription_type_id`
+                AND `subscription_types`.`length` >= ?
+            WHERE
+                `payment_items`.`type` IN (?)
+                AND `payments`.`user_id` BETWEEN ? AND ?
+            GROUP BY `payments`.`user_id`
+        SQL, self::PAYMENT_STATUSES, $paymentPaidAt, $this->minimalSubscriptionLength, $subscriptionTypeItem, $minUserId, $maxUserId)
             ->fetchPairs('user_id', 'subscription_payments');
 
         $this->userStatsRepository->upsertUsersValues('subscription_payments', $values);
@@ -159,19 +245,26 @@ SQL, self::PAYMENT_STATUSES, $subscriptionTypeItem, $minUserId, $maxUserId)
     {
         $this->line("  * computing '<info>subscription_payments_amount</info>' for user IDs between [<info>{$minUserId}</info>, <info>{$maxUserId}</info>]");
 
+        $paymentPaidAt = $this->startDate;
         $subscriptionTypeItem = SubscriptionTypePaymentItem::TYPE;
 
         $values = $this->database->query(<<<SQL
-                SELECT
-                    `payments`.`user_id` AS `user_id`,
-                    COALESCE(SUM(`payment_items`.`amount` * `payment_items`.`count`), 0) AS `subscription_payments_amount`
-                FROM `payment_items`
-                INNER JOIN `payments`
-                    ON `payments`.`id` = `payment_items`.`payment_id`
-                    AND `payments`.`status` IN (?)
-                WHERE `payment_items`.`type` IN (?) and `payments`.`user_id` BETWEEN ? AND ?
-                GROUP BY `payments`.`user_id`
-SQL, self::PAYMENT_STATUSES, $subscriptionTypeItem, $minUserId, $maxUserId)
+            SELECT
+                `payments`.`user_id` AS `user_id`,
+                COALESCE(SUM(`payment_items`.`amount` * `payment_items`.`count`), 0) AS `subscription_payments_amount`
+            FROM `payment_items`
+            INNER JOIN `payments`
+                ON `payments`.`id` = `payment_items`.`payment_id`
+                AND `payments`.`status` IN (?)
+                AND `payments`.`paid_at` > ?
+            INNER JOIN `subscription_types`
+                ON `subscription_types`.`id` = `payments`.`subscription_type_id`
+                AND `subscription_types`.`length` >= ?
+            WHERE
+                `payment_items`.`type` IN (?)
+                AND `payments`.`user_id` BETWEEN ? AND ?
+            GROUP BY `payments`.`user_id`
+        SQL, self::PAYMENT_STATUSES, $paymentPaidAt, $this->minimalSubscriptionLength, $subscriptionTypeItem, $minUserId, $maxUserId)
             ->fetchPairs('user_id', 'subscription_payments_amount');
 
         $this->userStatsRepository->upsertUsersValues('subscription_payments_amount', $values);
@@ -181,18 +274,22 @@ SQL, self::PAYMENT_STATUSES, $subscriptionTypeItem, $minUserId, $maxUserId)
     {
         $this->line("  * computing '<info>avg_month_payment</info>' for user IDs between [<info>{$minUserId}</info>, <info>{$maxUserId}</info>]");
 
+        $paymentPaidAt = $this->startDate;
+
         $values = $this->database->query(<<<SQL
-                SELECT
-                    `payments`.`user_id` AS `user_id`,
-                    COALESCE(AVG((`payments`.`amount` / `subscription_types`.`length`) * 31), 0) AS `avg_month_payment_amount`
-                FROM `payments`
-                INNER JOIN `subscription_types`
-                   ON `subscription_types`.`id` = `payments`.`subscription_type_id`
-                   AND `subscription_types`.`length` > 0
-                WHERE
-                    `payments`.`status` IN (?) AND `payments`.`user_id` BETWEEN ? AND ?
-                GROUP BY `payments`.`user_id`
-SQL, self::PAYMENT_STATUSES, $minUserId, $maxUserId)
+            SELECT
+                `payments`.`user_id` AS `user_id`,
+                COALESCE(AVG((`payments`.`amount` / `subscription_types`.`length`) * {$this->subscriptionPeriod}), 0) AS `avg_month_payment_amount`
+            FROM `payments`
+            INNER JOIN `subscription_types`
+                ON `subscription_types`.`id` = `payments`.`subscription_type_id`
+                AND `subscription_types`.`length` >= ?
+            WHERE
+                `payments`.`status` IN (?)
+                AND `payments`.`paid_at` > ?
+                AND `payments`.`user_id` BETWEEN ? AND ?
+            GROUP BY `payments`.`user_id`
+        SQL, $this->minimalSubscriptionLength, self::PAYMENT_STATUSES, $paymentPaidAt, $minUserId, $maxUserId)
             ->fetchPairs('user_id', 'avg_month_payment_amount');
 
         $this->userStatsRepository->upsertUsersValues('avg_month_payment', $values);
