7e717fbee08ef7edfb62dec80851529fbf09af98
Dominik Skerhak <dominik.skerhak@gmail.com>
2024-10-22 13:13:43 +0200

    Remove IUserGetter interface, use UserEventInterface
    IUserGetter was deprecated in version 2.3 (Feb 2023)
    https://github.com/remp2020/crm-users-module/releases/tag/2.3.0
    
    Use UserEventInterface instead (method `getUserId(): int` replaced
    by `getUser(): ActiveRow`.
    
    remp/crm#3313

--- src/Events/BaseRecurrentPaymentEvent.php
+++ src/Events/BaseRecurrentPaymentEvent.php
@@ -1,15 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Crm\PaymentsModule\Events;
 
-use Crm\UsersModule\Models\User\IUserGetter;
+use Crm\UsersModule\Events\UserEventInterface;
 use League\Event\AbstractEvent;
 use Nette\Database\Table\ActiveRow;
 
-abstract class BaseRecurrentPaymentEvent extends AbstractEvent implements IUserGetter, RecurrentPaymentEventInterface
+abstract class BaseRecurrentPaymentEvent extends AbstractEvent implements UserEventInterface, RecurrentPaymentEventInterface
 {
-    public function __construct(private ActiveRow $recurrentPayment)
-    {
+    public function __construct(
+        private ActiveRow $recurrentPayment,
+    ) {
     }
 
     public function getRecurrentPayment(): ActiveRow
@@ -17,8 +20,8 @@ abstract class BaseRecurrentPaymentEvent extends AbstractEvent implements IUserG
         return $this->recurrentPayment;
     }
 
-    public function getUserId(): int
+    public function getUser(): ActiveRow
     {
-        return $this->recurrentPayment->user_id;
+        return $this->recurrentPayment->user;
     }
 }
