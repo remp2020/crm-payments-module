71ace4afcf226576e352a974db18804ce4d4e4c7
Miroslav Svitok <mirosvitok@gmail.com>
2025-02-26 00:22:30 +0100

    Fix scenario payment status changed not being triggered when doing payment refund
    Problem was missing 'sendEmail' parameter in PaymentRefundFormFactory
    
    remp/helpdesk#3445

--- src/Forms/PaymentRefundFormFactory.php
+++ src/Forms/PaymentRefundFormFactory.php
@@ -129,7 +129,7 @@ class PaymentRefundFormFactory
             $this->stopRecurrentChargeInRefundedPayment($payment);
         }
 
-        $this->paymentsRepository->updateStatus($payment, $newPaymentStatus);
+        $this->paymentsRepository->updateStatus($payment, $newPaymentStatus, true);
 
         /** @var PaymentRefundFormDataProviderInterface[] $providers */
         $providers = $this->dataProviderManager->getProviders(
